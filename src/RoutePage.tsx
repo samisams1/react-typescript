685d6e311575453a18d3bca75a5f95f8d8f15c32
samisams1 <forsamisams@gmail.com>
2023-10-16 12:19:34 +0300

    chage product

--- src/RoutePage.tsx
+++ src/RoutePage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ShopeProduct } from './pages/product/shope/shopeProduct';
 const RoutePage: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<ShopeProduct />} />
         <Route path="/product" element={<ShopeProduct />} />
       </Routes>
     </Router>
