19aed047c07be7dc8b06e566b2b3b9c40c84f361
samisams1 <forsamisams@gmail.com>
2023-10-17 16:44:42 +0300

    comment the route

--- src/RoutePage.tsx
+++ src/RoutePage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { ShopeProduct } from './pages/product/shope/shopeProduct';
 const RoutePage: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ShopeProduct />} />
+    <Routes>
+
+      <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/product" element={<ShopeProduct />} />
-      </Routes>
-    </Router>
+    </Routes>
+  </Router>
   );
 };
 
