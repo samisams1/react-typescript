9eb440a771ac233e3c044411d4c6fec278c71493
samisams1 <forsamisams@gmail.com>
2023-10-17 16:56:30 +0300

    add product

--- src/RoutePage.tsx
+++ src/RoutePage.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { ShopeProduct } from './pages/product/shope/shopeProduct';
 const RoutePage: React.FC = () => {
   return (
     <Router>
-    <Routes>
-
-      <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/product" element={<ShopeProduct />} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/product" element={<h1>Product Page</h1>} />
+      </Routes>
+    </Router>
   );
 };
 
