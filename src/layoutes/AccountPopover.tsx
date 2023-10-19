ea98dd28671276ced1dd04ce61c4815ef9e1cbbe
samisams1 <forsamisams@gmail.com>
2023-10-19 11:44:56 +0300

    add some files

--- src/layoutes/AccountPopover.tsx
+++ src/layoutes/AccountPopover.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useContext } from 'react';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 // @mui
 import { alpha } from '@mui/material/styles';
 import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton, Button } from '@mui/material';
@@ -20,7 +20,7 @@ const MENU_OPTIONS = [
 // ----------------------------------------------------------------------
 export default function AccountPopover() {
   const anchorRef = useRef<HTMLButtonElement>(null);
-  const navigate = useNavigate();
+ // const navigate = useNavigate();
   const { currentUser } = useContext(UserContext);
   const [open, setOpen] = useState<HTMLButtonElement | null>(null);
   //const currentUser = AuthService.getCurrentUser();
